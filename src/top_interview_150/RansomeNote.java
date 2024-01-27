3a586ebafc53dfca22427c20bb2fa6deeaf3461e
prachisv04 <prachisvavhal@gmail.com>
2024-01-27 20:30:33 +0530

    383. Ransom Note

---
+++ src/top_interview_150/RansomeNote.java
@@ -0,0 +1,28 @@
+package top_interview_150;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class RansomeNote {
+	
+	public boolean canConstruct(String ransomNote, String magazine) {
+		 HashMap<Character,Integer> magazineFreq = new HashMap<>();
+
+	       
+	        for(char c:magazine.toCharArray() ){
+	            magazineFreq.put( c , magazineFreq.getOrDefault(c,0)+1 );
+	        }
+
+	        for(char c:ransomNote.toCharArray() ){
+	            if(magazineFreq.containsKey(c) && (magazineFreq.get(c)>0)){
+	               magazineFreq.put( c , magazineFreq.get(c)-1 );
+	            }
+	            else
+	            return false;
+	         
+	        }
+	      
+	        return true;
+
+    }
+}
