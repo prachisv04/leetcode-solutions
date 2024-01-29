224074813f7f1aa3dff770f2632db0bcb8b2f6a3
prachisv04 <prachisvavhal@gmail.com>
2024-01-29 22:50:56 +0530

    141. Linked List Cycle

---
+++ src/top_interview_150/LinkedListCycle.java
@@ -0,0 +1,26 @@
+package top_interview_150;
+
+public class LinkedListCycle {
+
+	class ListNode {
+		     int val;
+		     ListNode next;
+		     ListNode(int x) {
+		          val = x;
+		          next = null;
+		      }
+		 }
+	
+	public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head ;
+        while( fast != null  && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            return true;
+        }
+        return false;
+    }
+}
