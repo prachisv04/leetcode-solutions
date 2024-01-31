018674271509db882a03059c3c58364f4bfa0f9e
prachisv04 <prachisvavhal@gmail.com>
2024-01-31 20:17:12 +0530

    226. Invert Binary Tree

---
+++ src/top_interview_150/InvertBinaryTree.java
@@ -0,0 +1,29 @@
+package top_interview_150;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertBinaryTree {
+	private static void invert(TreeNode root) {
+	      if (root == null) return;
+	      TreeNode temp = root.right;
+	      root.right = root.left;
+	      root.left = temp;
+	    }
+	    public TreeNode invertTree(TreeNode root) {
+	        if (root == null) return null;
+	      Queue<TreeNode> q = new LinkedList<>();
+	      q.add(root);
+	      while (q.size() > 0) {
+	        int size = q.size();
+	        for (int i = 0; i < size; i++) {
+	          TreeNode temp = q.remove();
+	          invert(temp);
+	          if (temp.left != null) q.add(temp.left);
+	          if (temp.right != null) q.add(temp.right);
+	        }
+	      }
+	      return root;
+	    }
+
+}
