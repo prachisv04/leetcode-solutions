805eeeb620480d5c41416a64e9e7fb8fcb041aa5
prachisv04 <prachisvavhal@gmail.com>
2024-02-09 09:34:45 +0530

    11. Container with most water

---
+++ src/top_interview_150/ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+package top_interview_150;
+
+public class ContainerWithMostWater {
+	
+	public int maxArea(int[] height) {
+        int n = height.length, l = 0, r = n-1, maxArea = 0;
+        while(l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r-l));
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxArea;
+    }
+
+}
